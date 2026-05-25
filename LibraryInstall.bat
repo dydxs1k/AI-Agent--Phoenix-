144a9ae97c9c954040c27d5e73e5f2420f1a9394
DyDxS1k/The Disgrased One <dimakuzmenko06@gmail.com>
2026-05-25 13:23:17 +0300

    Обновления до версии 0.4 + исправление ошибок

--- LibraryInstall.bat
+++ LibraryInstall.bat
@@ -1,3 +1,7 @@
-pip insatll requests
-pip insatll duckduckgo-search
+pip install requests
+pip install duckduckgo-search
 winget install Ollama.Ollama
+pip install rich
+pip install beautifulsoup4
+pip install colorama
+pip install python-dotenv
